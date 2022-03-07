cee094368b4a9db4df96672fe643fd1a2d527205
Ivan Dubchak <miclarkson999@gmail.com>
2022-03-07 15:18:37 +0200

    add logs blocobserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+import 'package:dubchak_notes_app/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final storage = await HydratedStorage.build(
-      storageDirectory: await getTemporaryDirectory());
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
   HydratedBlocOverrides.runZoned(
     () => runApp(const NotesApp()),
     storage: storage,
-    // blocObserver: CounterObserver(),
+    blocObserver: MyBlocObserver(),
   );
 }
